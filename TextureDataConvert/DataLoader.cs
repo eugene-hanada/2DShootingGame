e076acdf00de76c2e1fe614193c59b25781943ed
eugene-hanada <76219486+eugene-hanada@users.noreply.github.com>
2022-07-06 00:09:50 +0900

    出力用C＃ファイルを追加

---
+++ TextureDataConvert/DataLoader.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
+
+namespace TextureDataConvert
+{
+	internal class DataLoader
+	{
+		private string fileName_;
+		private Dictionary<string,List<TextureData>> textureData_;
+
+		DataLoader(string fileName)
+		{
+			Load(fileName);
+		}
+
+		bool Load(string fnameName)
+		{
+			XElement xml;
+			try
+			{
+				xml = XElement.Load(fnameName);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				return false;
+			}
+
+			var img = xml.Attribute("imagePath");
+			if (img == null)
+			{
+				return false;
+			}
+
+			fileName_ = img.Value;
+
+			foreach (var sp in xml.Elements())
+			{
+				var nameAtr = sp.Attribute("n");
+				if (nameAtr != null)
+				{
+					var name = nameAtr.Value;
+					
+				}
+				
+			
+			}
+
+			return true;
+		}
+
+		
+		private class TextureData
+		{
+			private Vector2 pos_;
+			private Vector2 wh_;
+
+			public TextureData(ref XElement sprite)
+			{
+				var x = sprite.Attribute("x");
+				var y = sprite.Attribute("y");
+				if (x == null || y == null)
+				{
+					return;
+				}
+				pos_ = new Vector2(Int32.Parse(x.Value), Int32.Parse(y.Value));
+
+				var w = sprite.Attribute("w");
+				var h = sprite.Attribute("h");
+
+				if (w == null || h == null)
+				{
+					return;
+				}
+				wh_ = new Vector2(Int32.Parse(w.Value), Int32.Parse(h.Value));
+
+			}
+
+		}
+		
+	}
+}
---
+++ TextureDataConvert/Vector2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Buffers.Binary;
+
+
+namespace TextureDataConvert
+{
+	internal class Vector2
+	{
+		public float x;
+		public float y;
+		public Vector2(float x, float y)
+		{
+			this.x = x; 
+			this.y = y;
+		}
+
+		public Vector2(Vector2 vector2)
+		{
+			this.x = vector2.x;
+			this.y = vector2.y;
+		}
+
+		void Write(BinaryWriter bw)
+		{
+			bw.Write(x);
+			bw.Write(y);
+		}
+
+	}
+}
