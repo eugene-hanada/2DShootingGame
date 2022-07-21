747f0a4b84ce67305ed0ab1770c062ba36494ec4
eugene-hanada <76219486+eugene-hanada@users.noreply.github.com>
2022-07-21 23:53:39 +0900

    アニメーションデータの変換用のプログラムを追加

---
+++ ConvertAnimationData/AnimationData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertAnimationData
+{
+    internal class AnimationData
+    {
+        string name_;
+        Dictionary<string,int> values_;
+
+    }
+}
